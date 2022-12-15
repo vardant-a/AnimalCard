83bb202494ee543028cf4af93321aa591cd4891e
vardant <lokhin.a.a@gmail.com>
2022-12-15 22:20:35 +0300

    Initial Commit

---
+++ AnimalCard/AnimalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCardApp.swift
+//  AnimalCard
+//
+//  Created by Aleksei on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
